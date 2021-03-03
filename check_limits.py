fbff45c834ddd5d99ccd9b6e4715af50b763e738
Anjana-MU <77484700+Anjana-MU@users.noreply.github.com>
2021-03-03 07:44:39 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,18 +1,33 @@
-
-def battery_is_ok(temperature, soc, charge_rate):
-  if temperature < 0 or temperature > 45:
-    print('Temperature is out of range!')
-    return False
-  elif soc < 20 or soc > 80:
-    print('State of Charge is out of range!')
-    return False
-  elif charge_rate > 0.8:
-    print('Charge rate is out of range!')
-    return False
-
-  return True
+bms_attribute_limits = {
+     'temperature': {'min': 0, 'max': 45},
+     'soc': {'min': 20, 'max': 80},
+     'charge_rate': {'min': 0,'max': 0.8}
+        }
 
 
+def check_attributes_limit(bms_attribute):
+    offLimits = []
+    for bms_name,bms_value in bms_attribute.items() :
+        compare_parameter_values(bms_name,bms_value,offLimits)       
+    return offLimits
+
+def compare_parameter_values(bms_name,bms_value,offLimits):
+     if (bms_value < bms_attribute_limits[bms_name]['min']) or (bms_value > bms_attribute_limits[bms_name]['max']):
+            offLimits.append(bms_name)
+
+
+def battery_is_ok(bms_attribute):
+    bms_attributes_check = check_attributes_limit(bms_attribute)
+    if len(bms_attributes_check) == 0:
+        print("Battery is ok")
+        return True
+    else :
+        print("Battery is not ok")
+        return False
+    
 if __name__ == '__main__':
-  assert(battery_is_ok(25, 70, 0.7) is True)
-  assert(battery_is_ok(50, 85, 0) is False)
+  assert(battery_is_ok({'temperature': 25,'soc': 70, 'charge_rate': 0.7}) is True)  #all attributes fine
+  assert(battery_is_ok({'temperature': 50,'soc': 85, 'charge_rate': 0}) is False)  #all attributes off limit
+  assert(battery_is_ok({'temperature': 100,'soc': 30, 'charge_rate': 0.5}) is False) #temp off limit
+  assert(battery_is_ok({'temperature': 25,'soc': 10, 'charge_rate': 0.5}) is False) #SOC off limit
+  assert(battery_is_ok({'temperature': 25,'soc': 70, 'charge_rate': 1}) is False) #charge off limit    
