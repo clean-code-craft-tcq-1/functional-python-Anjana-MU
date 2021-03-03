72005802e74df811257f0fa825de2203da01d0d7
Anjana-MU <77484700+Anjana-MU@users.noreply.github.com>
2021-03-03 10:30:33 +0530

    Update report_vitals.py

--- report_vitals.py
+++ report_vitals.py
@@ -2,4 +2,6 @@ def report_normal_vitals():
     print("Battery is Ok")
     
 def report_abnormal_vitals(bms_vitals):
-    print("Abnormal Vitals",bms_vitals)
+    print("Abnormal Vitals")
+    for vitals in bms_vitals:
+        print(vitals)  
